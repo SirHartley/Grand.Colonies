3e7d17554beea0693c26a501e0a5fd85788ff590
Philipp <philipp.hartl@zetaworks.at>
2023-06-29 23:32:39 +0200

    fixed cramped condition stuff

--- jars/src/grandcolonies/listeners/CrampedConditionApplicator.java
+++ jars/src/grandcolonies/listeners/CrampedConditionApplicator.java
@@ -2,11 +2,13 @@ package grandcolonies.listeners;
 
 import com.fs.starfarer.api.campaign.BaseCampaignEventListener;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Factions;
 import grandcolonies.conditions.CrampedCondition;
 
-public class CrampedConditionApplicator extends BaseCampaignEventListener {
+import java.util.ArrayList;
 
+public class CrampedConditionApplicator extends BaseCampaignEventListener {
     public CrampedConditionApplicator() {
         super(false);
     }
@@ -14,6 +16,7 @@ public class CrampedConditionApplicator extends BaseCampaignEventListener {
     @Override
     public void reportPlayerOpenedMarket(MarketAPI market) {
         super.reportPlayerOpenedMarket(market);
+
         if (market.isPlayerOwned() || Factions.PLAYER.equals(market.getFactionId())) {
             if (!market.hasCondition(CrampedCondition.CONDITION_ID)) market.addCondition(CrampedCondition.CONDITION_ID);
         }
--- jars/src/grandcolonies/plugins/ModPlugin.java
+++ jars/src/grandcolonies/plugins/ModPlugin.java
@@ -3,13 +3,17 @@ package grandcolonies.plugins;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import grandcolonies.conditions.CrampedCondition;
 import grandcolonies.listeners.CrampedConditionApplicator;
 import grandcolonies.listeners.IndustryPanelReplacer;
 import grandcolonies.listeners.PageMovingOptionProvider;
 import grandcolonies.listeners.PlayerOpenedMarketListener;
 import grandcolonies.memory.PageMemory;
 import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
 
 public class ModPlugin extends BaseModPlugin {
 
@@ -21,6 +25,8 @@ public class ModPlugin extends BaseModPlugin {
     public static final String DOCKED_KEY = "$GrandColonies_docked";
     public static final String EXCLUDED_IDS = "$GrandColonies_excl";
     public static final String WAS_LEGACY_MODE = "$GrandColonies_legacy";
+
+    public static final String FIXED_MARKET_CONDITIONS = "$GrandColonies_fixedMarketConditions";
     
     public static void log(String s){
         log.info(s);
@@ -31,6 +37,8 @@ public class ModPlugin extends BaseModPlugin {
         super.onGameLoad(newGame);
         MemoryAPI mem = Global.getSector().getMemoryWithoutUpdate();
 
+        fixMarketConditions(); //remove next ver
+
         if (LEGACY_MODE) {
             mem.set(WAS_LEGACY_MODE, true);
             PageMovingOptionProvider.register();
@@ -40,6 +48,20 @@ public class ModPlugin extends BaseModPlugin {
             if (!mem.contains(WAS_LEGACY_MODE) || mem.getBoolean(WAS_LEGACY_MODE)) cleanupAll();
             Global.getSector().addTransientListener(new CrampedConditionApplicator());
             Global.getSector().addTransientScript(new IndustryPanelReplacer());
+        }
+    }
+
+    public void fixMarketConditions(){
+        MemoryAPI mem = Global.getSector().getMemoryWithoutUpdate();
+
+        if (!mem.getBoolean(FIXED_MARKET_CONDITIONS)){
+            for (MarketAPI m : Global.getSector().getEconomy().getMarketsCopy()){
+                for (MarketConditionAPI cond : new ArrayList<>(m.getConditions())){
+                    if (cond.getSpec().getId().equals(CrampedCondition.CONDITION_ID)) m.removeSpecificCondition(cond.getIdForPluginModifications());
+                }
+            }
+
+            mem.set(FIXED_MARKET_CONDITIONS, true);
         }
     }
 
